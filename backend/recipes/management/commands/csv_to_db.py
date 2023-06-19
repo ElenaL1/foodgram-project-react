8f3ffd495152534de1c50d06adef67ce504dba1b
Elena Lambert <elena_lambert@yahoo.com>
2023-06-19 13:04:25 +0300

    2 deploy

--- backend/recipes/management/commands/csv_to_db.py
+++ backend/recipes/management/commands/csv_to_db.py
@@ -17,7 +17,8 @@ class Command(BaseCommand):
     help = 'Команда для переноса данных из csv файла в БД'
 
     def handle(self, *args, **options):
-        with open(os.path.join(os.path.dirname(settings.BASE_DIR),
+        # with open(os.path.join(os.path.dirname(settings.BASE_DIR),
+        with open(os.path.join((settings.BASE_DIR),
                   'data', 'ingredients.csv'),
                   encoding='utf-8') as csv_file:
             reader = csv.DictReader(csv_file,
