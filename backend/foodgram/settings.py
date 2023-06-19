b98325fe85ead9cc0ddda087444c54f9b0c1c0e6
Elena Lambert <elena_lambert@yahoo.com>
2023-06-19 12:49:30 +0300

    2 deploy

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY',)
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ['https://localhost', 'https://158.160.44.210', 'https://127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://localhost', '158.160.44.210', 'https://127.0.0.1']
 INTERNAL_IPS = ['158.160.44.210', ]
 
 
