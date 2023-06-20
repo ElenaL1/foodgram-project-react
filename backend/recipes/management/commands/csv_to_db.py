b3cb97e338f5eb1ffb7615b96f02aa4fe4c9af66
Elena Lambert <elena_lambert@yahoo.com>
2023-06-20 20:28:06 +0300

    Correct path in csv

--- backend/recipes/management/commands/csv_to_db.py
+++ backend/recipes/management/commands/csv_to_db.py
@@ -17,10 +17,8 @@ class Command(BaseCommand):
     help = 'Команда для переноса данных из csv файла в БД'
 
     def handle(self, *args, **options):
-        # with open(os.path.join(os.path.dirname(settings.BASE_DIR),
-        #          'data', 'ingredients.csv'),
-        with open(os.path.join((settings.BASE_DIR),
-                  'ingredients.csv'),
+        with open(os.path.join(os.path.dirname(settings.BASE_DIR),
+                  'data', 'ingredients.csv'),
                   encoding='utf-8') as csv_file:
             reader = csv.DictReader(csv_file,
                                     fieldnames=['name', 'measurement_unit'])
